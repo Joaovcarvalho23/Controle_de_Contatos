823ead6d0f99b5d3cb8d3337726d8fb2abdd34b2
Joao <joaovictorcordeiro00@gmail.com>
2023-10-03 09:08:21 -0300

    Fazendo layout com tabelas estáticas

---
+++ Controle_de_Contatos/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controle_de_Contatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
