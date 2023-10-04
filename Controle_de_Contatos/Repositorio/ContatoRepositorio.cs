815c107c04f92a0dea2a67dd3f74074596a50652
Joao <joaovictorcordeiro00@gmail.com>
2023-10-04 08:34:24 -0300

    Criando pasta para repositorio do contato e sua interface

---
+++ Controle_de_Contatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,24 @@
+﻿using Controle_de_Contatos.Data;
+using Controle_de_Contatos.Models;
+
+namespace Controle_de_Contatos.Repositorio
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        private readonly BancoDeDadosContext _bancoDeDadosContext; //atributo para poder fazer a injeção
+
+        public ContatoRepositorio(BancoDeDadosContext bancoDeDadosContext) //construtor
+        {
+            _bancoDeDadosContext = bancoDeDadosContext;
+        }
+
+
+        public ContatoModel Adicionar(ContatoModel contato) //gravar no banco de dados. Porém, quem vai gravar propriamente dito é o contexto, que está dentor de Data. Então, esse contexto deve ser injetado para dentro do ContatoRepositorio 
+        {
+            _bancoDeDadosContext.TabelaContatos.Add(contato);
+            _bancoDeDadosContext.SaveChanges();
+
+            return contato;
+        }
+    }
+}
---
+++ Controle_de_Contatos/Repositorio/IContatoRepositorio.cs
@@ -0,0 +1,9 @@
+﻿using Controle_de_Contatos.Models;
+
+namespace Controle_de_Contatos.Repositorio
+{
+    public interface IContatoRepositorio
+    {
+        ContatoModel Adicionar(ContatoModel contato);
+    }
+}
