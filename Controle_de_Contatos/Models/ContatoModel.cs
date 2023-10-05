f0b5cc737b9d6db9f6a8e1363adceb8ee3779b47
Joao <joaovictorcordeiro00@gmail.com>
2023-10-05 10:06:29 -0300

    Modificando o model para fazer validações

--- Controle_de_Contatos/Models/ContatoModel.cs
+++ Controle_de_Contatos/Models/ContatoModel.cs
@@ -1,10 +1,20 @@
-﻿namespace Controle_de_Contatos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Controle_de_Contatos.Models
 {
     public class ContatoModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o nome do contato")]
         public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o e-mail do contato")]
+        [EmailAddress(ErrorMessage = "Formato de e-mail inválido!")]
         public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o celular do contato")]
+        [Phone(ErrorMessage = "Formato do celular está incorreto!")]
         public string? Celular { get; set;}
     }
 }
