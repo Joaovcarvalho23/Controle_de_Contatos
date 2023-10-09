cf7359924c4ff652cc38ba8b2fd57dc8fc44264e
Joao <joaovictorcordeiro00@gmail.com>
2023-10-09 09:17:45 -0300

    iniciando implementação do modelo de Usuário

---
+++ Controle_de_Contatos/Enums/PerfilEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Controle_de_Contatos.Enums
+{
+    public enum PerfilEnum
+    {
+        Admin = 1,
+        Padrao = 2
+    }
+}
---
+++ Controle_de_Contatos/Models/UsuarioModel.cs
@@ -0,0 +1,13 @@
+﻿using Controle_de_Contatos.Enums;
+
+namespace Controle_de_Contatos.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Login { get; set;}
+        public PerfilEnum Perfil { get; set; }
+    }
+}
