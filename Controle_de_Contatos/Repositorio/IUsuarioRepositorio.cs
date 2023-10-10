7508a94a0bebdf5d95133a2e795c4561f8df5be7
Joao <joaovictorcordeiro00@gmail.com>
2023-10-10 09:35:25 -0300

    criada a interface que será implementada pelo repositorio de usuario

---
+++ Controle_de_Contatos/Repositorio/IUsuarioRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using Controle_de_Contatos.Models;
+
+namespace Controle_de_Contatos.Repositorio
+{
+    public interface IUsuarioRepositorio
+    {
+        UsuarioModel BuscarPorId(int id);
+
+        List<UsuarioModel> BuscarTodosUsuarios();
+
+        UsuarioModel Adicionar(UsuarioModel usuario);
+
+        UsuarioModel Atualizar (UsuarioModel usuario);
+
+        bool Deletar(int id);
+    }
+}
---
+++ Controle_de_Contatos/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,73 @@
+﻿using Controle_de_Contatos.Data;
+using Controle_de_Contatos.Models;
+
+namespace Controle_de_Contatos.Repositorio
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly BancoDeDadosContext _bancoDeDadosContext; //atributo
+        
+        public UsuarioRepositorio(BancoDeDadosContext bancoDeDadosContext) //construtor para fazermos a injeção 
+        {
+            _bancoDeDadosContext = bancoDeDadosContext;
+        }
+
+
+        //métodos
+        public UsuarioModel BuscarPorId(int id)
+        {
+            return _bancoDeDadosContext.TabelaUsuarios.FirstOrDefault(o => o.Id == id);
+        }
+
+
+
+        public List<UsuarioModel> BuscarTodosUsuarios()
+        {
+            return _bancoDeDadosContext.TabelaUsuarios.ToList();
+        }
+
+
+        
+        public UsuarioModel Adicionar(UsuarioModel usuario)
+        {
+            _bancoDeDadosContext.TabelaUsuarios.Add(usuario);
+            _bancoDeDadosContext.SaveChanges();
+
+            return usuario;
+        }
+
+
+
+        public UsuarioModel Atualizar(UsuarioModel usuario)
+        {
+            UsuarioModel usuarioDB = BuscarPorId(usuario.Id);
+
+            if(usuarioDB != null) { throw new Exception("Ocorreu um erro ao atualizar o usuário"); }
+
+            usuarioDB.Name = usuario.Name;
+            usuarioDB.Login = usuario.Login;
+            usuarioDB.Email = usuario.Email;
+
+            _bancoDeDadosContext.TabelaUsuarios.Update(usuarioDB);
+            _bancoDeDadosContext.SaveChanges();
+
+            return usuarioDB;
+
+        }
+
+
+        
+        public bool Deletar(int id)
+        {
+            UsuarioModel usuarioDB = BuscarPorId(id);
+
+            if (usuarioDB == null)
+                throw new Exception("Ocorreu um erro na exclusão do usuário");
+
+            _bancoDeDadosContext.TabelaUsuarios.Remove(usuarioDB);
+            _bancoDeDadosContext.SaveChanges();
+
+            return true;
+        }
+    }
+}
