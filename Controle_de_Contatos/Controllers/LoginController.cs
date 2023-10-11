218566c1fc7c4725b33ec29c95cfe4df3137f52f
Joao <joaovictorcordeiro00@gmail.com>
2023-10-11 11:48:07 -0300

    criando método para efetuar login do usuário

--- Controle_de_Contatos/Controllers/LoginController.cs
+++ Controle_de_Contatos/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Controle_de_Contatos.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Controle_de_Contatos.Controllers
 {
@@ -7,6 +8,27 @@ namespace Controle_de_Contatos.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Entrar(LoginModel loginModel)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                return View("Index");
+
+            }
+            catch (Exception ex)
+            {
+                TempData["MensagemFalha"] = $"Houve um erro ao tentar efetuar o login. Descrição do erro: {ex.Message}";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
