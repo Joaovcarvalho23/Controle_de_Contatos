5000395ebaee59f3b8c3f4344542a83689344173
Joao <joaovictorcordeiro00@gmail.com>
2023-10-11 11:42:40 -0300

    Criando Model para o login

---
+++ Controle_de_Contatos/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Controle_de_Contatos.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Por favor, informe o login")]
+        public string? Login {  get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe a senha")]
+        public string? Senha { get; set; }
+    }
+}
