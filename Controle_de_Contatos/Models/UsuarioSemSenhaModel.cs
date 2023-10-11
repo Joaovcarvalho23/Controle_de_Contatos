562369d14ef382253040dae225ca0c240dc74645
Joao <joaovictorcordeiro00@gmail.com>
2023-10-11 09:17:42 -0300

    fazendo modelo para um usuário sem senha, para poder editar o mesmo

---
+++ Controle_de_Contatos/Models/UsuarioSemSenhaModel.cs
@@ -0,0 +1,23 @@
+﻿using Controle_de_Contatos.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Controle_de_Contatos.Models
+{
+    public class UsuarioSemSenhaModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do usuário")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Digite o e-mail do usuário")]
+        [EmailAddress(ErrorMessage = "O formato de e-mail informado não é válido!")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Digite o e-mail do usuário")]
+        public string? Login { get; set;}
+
+        [Required(ErrorMessage = "Informe o perfil do usuário!")]
+        public PerfilEnum? Perfil { get; set; }
+    }
+}
