084780202a7b65b6e80a6f262a029d7ed91a269a
Joao <joaovictorcordeiro00@gmail.com>
2023-10-17 08:18:06 -0300

    Criando contrato na interface de Sessão de Usuário

---
+++ Controle_de_Contatos/Helper/ISessao.cs
@@ -0,0 +1,13 @@
+﻿using Controle_de_Contatos.Models;
+
+namespace Controle_de_Contatos.Helper
+{
+    public interface ISessao
+    {
+        //Criando contratos
+        void CriarSessaoUsuario(UsuarioModel usuario);
+        void RemoverSessaoUsuario();
+        UsuarioModel BuscarSessaoUsuario();
+
+    }
+}
