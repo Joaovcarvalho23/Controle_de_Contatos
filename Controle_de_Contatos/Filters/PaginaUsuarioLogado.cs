2356d94558a9a5b8bbc0cb2e74ee33b6f766c1c6
Joao <joaovictorcordeiro00@gmail.com>
2023-10-17 11:17:41 -0300

    criando controle de rotas de usuário, verificando se o mesmo tem acesso ou não

---
+++ Controle_de_Contatos/Filters/PaginaUsuarioLogado.cs
@@ -0,0 +1,33 @@
+﻿using Controle_de_Contatos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text.Json;
+
+namespace Controle_de_Contatos.Filters
+{
+    public class PaginaUsuarioLogado : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            string sessaoUsuario = context.HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if (string.IsNullOrEmpty(sessaoUsuario))
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "Index" } });
+                //se a sessão do usuário for nula ou vazia, chama o resultado do nosso contexto vai ser um redirect para a rota da controller Login e da action Index
+            }
+            else
+            {
+                UsuarioModel usuario = JsonSerializer.Deserialize<UsuarioModel>(sessaoUsuario);
+
+                //se por algum motivo ele não conseguiu serializar o objeto para o UsuarioModel
+                if(usuario == null)
+                {
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "Index" } });
+                }
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
