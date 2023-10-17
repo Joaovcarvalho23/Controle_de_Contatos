0c32628200d2897e622fbf252675b50d16495e4a
Joao <joaovictorcordeiro00@gmail.com>
2023-10-17 10:36:16 -0300

    Criando componente de Menu que será chamado de acordo com o login

---
+++ Controle_de_Contatos/ViewComponents/Menu.cs
@@ -0,0 +1,22 @@
+﻿using Controle_de_Contatos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace Controle_de_Contatos.ViewComponents
+{
+    public class Menu : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string? sessaoUsuario = HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if (string.IsNullOrEmpty(sessaoUsuario))
+                return null;
+
+            UsuarioModel usuario = JsonSerializer.Deserialize<UsuarioModel>(sessaoUsuario);
+
+            return View(usuario);
+        }
+    }
+}
