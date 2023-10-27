f49af962d00fed80a7aae9a00590514076319785
Joao <joaovictorcordeiro00@gmail.com>
2023-10-27 10:36:57 -0300

    Criando interface para enviar Email com nova senha e Classe implementando essa interface

---
+++ Controle_de_Contatos/Helper/Email.cs
@@ -0,0 +1,10 @@
+﻿namespace Controle_de_Contatos.Helper
+{
+    public class Email : IEmail
+    {
+        public bool Enviar(string email, string assunto, string mensagem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
