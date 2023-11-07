34bd908a0a06458df50245c624cdb60185102d99
Joao <joaovictorcordeiro00@gmail.com>
2023-11-07 11:45:13 -0300

    Criando Model para alterar senha do usuário

---
+++ Controle_de_Contatos/Models/AlterarSenhaModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Controle_de_Contatos.Models
+{
+    public class AlterarSenhaModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite a senha atual do usuário")]
+        public string? SenhaAtual { get; set; }
+
+        [Required(ErrorMessage = "Digite a nova senha do usuário")]
+        public string? NovaSenha {  get; set; }
+
+        [Required(ErrorMessage = "Confirmar nova senha do usuário")]
+        [Compare("NovaSenha", ErrorMessage = "Senha não confere com a nova senha")]
+        public string? ConfirmarNovaSenha { get; set; }
+    }
+}
