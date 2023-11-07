2fb3793b946d61e32a18caf4585f8017f7661e1a
Joao <joaovictorcordeiro00@gmail.com>
2023-11-07 11:45:43 -0300

    Criando controller para alterar senha do usuário

---
+++ Controle_de_Contatos/Controllers/AlterarSenhaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controle_de_Contatos.Controllers
+{
+    public class AlterarSenhaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
