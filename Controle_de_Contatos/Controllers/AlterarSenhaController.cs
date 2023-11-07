d8e76fb66bdd01eceaa051b0572a07e16785ad9d
Joao <joaovictorcordeiro00@gmail.com>
2023-11-07 12:03:57 -0300

    Criando método de alterar a senha dentro da controller

--- Controle_de_Contatos/Controllers/AlterarSenhaController.cs
+++ Controle_de_Contatos/Controllers/AlterarSenhaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Controle_de_Contatos.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Controle_de_Contatos.Controllers
 {
@@ -7,6 +8,26 @@ namespace Controle_de_Contatos.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(AlterarSenhaModel alterarSenhaModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    TempData["MensagemSucesso"] = "Senha alterada com sucesso!";
+                    return View("Index", alterarSenhaModel);
+                }
+
+                return View("Index", alterarSenhaModel);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemFalha"] = $"Não foi possível aterar a senha do usuário. Descrição do erro: {erro.Message}";
+                return View("Index", alterarSenhaModel);
+            }
         }
     }
 }
