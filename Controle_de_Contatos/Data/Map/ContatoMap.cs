fa4f3b06773a0a4780673fcec9d1f31ada63cb38
Joao <joaovictorcordeiro00@gmail.com>
2023-11-08 09:22:20 -0300

    Criando a pasta Map para fazermos o mapeamento de Contatos a um Usuario

---
+++ Controle_de_Contatos/Data/Map/ContatoMap.cs
@@ -0,0 +1,15 @@
+﻿using Controle_de_Contatos.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Controle_de_Contatos.Data.Map
+{
+    public class ContatoMap : IEntityTypeConfiguration<ContatoModel>
+    {
+        public void Configure(EntityTypeBuilder<ContatoModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasOne(x => x.Usuario);
+        }
+    }
+}
